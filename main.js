d886e3e5e0730f30532ed02f83c81cadb5e94dae
runningtalus <dave@davemay.org>
2014-04-18 13:50:32 -0600

    Bottom display:am, fm, and slider, positioned

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
 	
 	$('.container').css({
-		backgroundColor: 'dimgray',
+		backgroundColor: 'darkslategray',
 		height: '600px',
 		position: 'relative',
 		width: '900px'
 	});
 
 	var clock = $('<div class = "clock">');
-	
 	$('.container').append(clock);
-
 	$('.clock').css({
 		backgroundColor: 'black',
 		borderRadius: '50px',
@@ -22,9 +20,7 @@ $(document).ready(function(){
 	});
 
 	var display = $('<div class = "display">');
-	
 	$('.clock').append(display);
-
 	$('.display').css({
 		backgroundColor: 'firebrick',
 		borderRadius: '50px',
@@ -34,6 +30,57 @@ $(document).ready(function(){
 		top: '75px',
 		width: '550px'
 	});
+
+	var bottomDisplay = $('<div class = "bottomDisplay">');
+	$('.display').append(bottomDisplay);
+	$('.bottomDisplay').css({
+		border: 'solid',
+		color: 'purple',
+		height: '175px',
+		left: '-25px',
+		top: '300px',
+		position: 'absolute',
+		width: '600px'
+	});
+
+	var am = $('<div class = "am">');
+	$('.bottomDisplay').append(am);
+	$('.am').css({
+		border: 'solid',
+		color: 'turquoise',
+		height: '60px',
+		position: 'absolute',
+		width: '594px'
+
+	});
+
+	var fm = $('<div class = "fm">');
+	$('.am').append(fm);
+	$('.fm').css({
+		border: 'solid',
+		color: 'chartreuse',
+		height: '60px',
+		left: '-3px',
+		position: 'absolute',
+		top: '60px',
+		width: '594px'
+	});
+	
+	var slider = $('<div class = "slider">');
+	$('.fm').append(slider);
+	$('.slider').css({
+		border: 'solid',
+		color: 'deeppink',
+		height: '40px',
+		left: '-3px',
+		position: 'absolute',
+		top: '60px',
+		width: '594px'
+	});
+
+
+
+
 
 
 
